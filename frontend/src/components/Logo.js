7b37b48b55af394ee2bba884a3e0a8affbf36998
siam chowdhury <smsiam696@gmail.com>
2024-08-08 23:58:41 +0600

    first

---
+++ frontend/src/components/Logo.js
@@ -0,0 +1,8 @@
+import React from "react";
+import logo from "../assest/nsr_mart.png";
+
+const Logo = ({ w, h }) => {
+  return <img src={logo} alt="Logo" className="w-20" />;
+};
+
+export default Logo;
